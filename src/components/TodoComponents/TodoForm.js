185f938dbb7f6065379c7f2f286ad28bf1cd3c77
joe-alfaro <josephlalfaro@gmail.com>
2019-03-20 13:38:07 -0700

    set up form component

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 import './Todo.css'
 
-const TodoForm = () => {
+const TodoForm = props => {
   return (
     <form className="todo-form">
-      <input type="text" placeholder="...to do" />
-      <button className="add-todo">Add To Do</button>
-      <button className="clear-completed">Clear Completed</button>
+      <input 
+        type="text"
+        value={props.todoItem}
+        name="todo-item"
+        placeholder="...to do" 
+        onChange={props.handleChanges} 
+      />
+      <button onClick={props.updateList}>Add To Do</button>
+      <button onClick={props.clearCompleted}>Clear Completed</button>
     </form>
   );
 };
